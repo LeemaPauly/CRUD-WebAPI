a6302010d05a4fc5df1aefb73127ae629afcd4e3
Leema Pauly <leemapauly22@gmail.com>
2024-10-11 16:54:39 +0530

    crud api complete solution

---
+++ DAL/MyAppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using CRUD_WebAPI.Model;
+using Microsoft.EntityFrameworkCore;
+
+namespace CRUD_WebAPI.DAL
+{
+    public class MyAppDbContext : DbContext
+    {
+        public MyAppDbContext(DbContextOptions options) : base(options)
+        {
+        }
+        public DbSet<Product> Products { get; set; }
+    }
+}
